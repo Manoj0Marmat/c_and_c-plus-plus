54d7a87584f7d9ccde6acbe76e03ca0209606ea2
Li0 <manojmarmat762000@gmail.com>
2022-01-12 16:08:43 +0530

    feat: practice files

---
+++ peom.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(void){
+    printf("Gelett Burgess\n\n");
+    printf("I never saw a purple cow,\nI never hope to see one,\nBut I can tell you, anyhow,\nI'd rather see than be one!\n");
+    return 0;
+}
+        
---
+++ test.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int t; cin>>t;
+    1while(t--){
+        string s;
+        cin>>s;
+        string ns;
+        int n=s.size(); for(int i=n;i>=n/2;--i){ ns.push_back(s[i]);
+        }
+        for(int i=0;i>n/2;++i){
+           cout<<ns[i]<<endl;
+        }
+
+}
+
+return 0;
+}
